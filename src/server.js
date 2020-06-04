9b5f5861103deede70ce34c7c5a2ec6381a342ac
Henrique Paraguassu <henriquevoglerparaguassu@gmail.com>
2020-06-04 15:22:32 -0400

    add user controller

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const routes = require('./routes')
+
 const app = express()
+app.use(routes)
 
 app.listen(3333, () => {
-  console.log('Running on localhost:3333/')
+  console.log('Running on localhost:3333')
 })
